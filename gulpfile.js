a51d85b94a55cac441e828aeca18c1c175d16c62
imrankhan <imrankhan>
2018-02-15 13:20:59 +0530

    hey

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+postcssnested = require('postcss-nested'),
+postImport = require('postcss-import');
 
-gulp.task('default', function() {
-    console.log("Horray gulp running");
+gulp.task('default', function(){
+    console.log("hey world");
 });
 
-gulp.task('html', function() {
-    console.log("hey hey")
-})
+gulp.task('html', function(){
+    console.log("html changed");
+});
+
+gulp.task('css', function(){
+   return gulp.src('./app/assets/styles/styles.css')
+   .pipe(postcss([postImport, postcssnested, cssvars, autoprefixer]))
+   .pipe(gulp.dest('./app/temp/styles/'));
+});
+
+gulp.task('watch', function(){
+    watch('./app/index.html', function() {
+        gulp.start('html');
+    });
+
+    watch('./app/assets/**/*.css', function() {
+        gulp.start('css');
+    });
+});
